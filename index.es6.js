7ccfd5a112a5f397b15d212211f7f507627594bb
mo <feng_zilong@163.com>
2016-05-21 17:41:58 +0800

    :zap:date util

--- index.es6.js
+++ index.es6.js
@@ -2,7 +2,7 @@ import FETCH_LIKE from './api/TIEBA_FETCH_LIKE';
 import SIGN_ALL from './api/TIEBA_SIGN_ALL';
 import USER_PROFILE from './api/TIEBA_USER_PROFILE';
 import { SIGN_CONF_PATH } from './config';
-import hash from './hash';
+import date from './util/date';
 import es6promise from 'es6-promise';
 import fs from 'fs';
 es6promise.polyfill();
@@ -20,9 +20,11 @@ SIGN_ALL.on('signed', ( name, i ) => {
 	console.log( `${i+1}、${name} 已签到` );
 });
 
+let username;
 USER_PROFILE()
 	.then(profile => {
-		console.log( `开始用户"${profile.user_name_show}"的签到` );
+		username = profile.user_name_show;
+		console.log( `开始用户"${username}"的签到` );
 	}, () => {
 		return Promise.reject();
 	})
@@ -37,9 +39,7 @@ USER_PROFILE()
 		}
 		return names;
 	})
-	.then(names => {
-		return SIGN_ALL( names );
-	})
+	.then(names => SIGN_ALL( names ))
 	.then(({ signNotSupported, signFailed, signSuccess, signed }) => {
 		console.log( `无法签到：${signNotSupported.length}` );
 		console.log( `签到成功：${signed.length + signSuccess.length}` );
